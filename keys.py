92a69dd762c83ea96d0fc2e02482131583d4a889
Kyle Java <java002@cougars.csusm.edu>
2020-12-14 19:41:01 -0800

    Delete keys.py

--- keys.py
+++
@@ -1,4 +0,0 @@
-endpoint = r"https://kylejava.cognitiveservices.azure.com/"
-post_url = endpoint + "/formrecognizer/v2.0/Layout/analyze"
-apim_key = r"164b0043fac247ea8749594d4f6ede30"
-source = r"receipt.pdf"
