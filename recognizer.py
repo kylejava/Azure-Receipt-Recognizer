f5dd90223b1adbfbf66c9b46c1425a8e7b738f61
kylejava <kyleedwardgjava@gmail.com>
2021-01-04 19:29:00 -0800

    Put contents of receipt in list

--- recognizer.py
+++ recognizer.py
@@ -4,28 +4,26 @@ from requests import get, post
 from keys import *
 from pprint import pprint
 
-def x():
+def getTotal():
+    text = []
     headers = {
         # Request headers
         'Content-Type': 'application/pdf',
         'Ocp-Apim-Subscription-Key': apim_key,
     }
-
     with open(source, "rb") as f:
         data_bytes = f.read()
-
-
     try:
         resp = post(url = post_url, data = data_bytes, headers = headers)
         if resp.status_code != 202:
             print("POST analyze failed:\n%s" % resp.text)
             quit()
-        print("POST analyze succeeded:\n%s" % resp.headers)
+    #print("POST analyze succeeded:\n%s" % resp.headers)
         get_url = resp.headers["operation-location"]
     except Exception as e:
         print("POST analyze failed:\n%s" % str(e))
         quit()
-    data = []
+
     n_tries = 10
     n_try = 0
     wait_sec = 6
@@ -38,8 +36,9 @@ def x():
                 quit()
             status = resp_json["status"]
             if status == "succeeded":
-                for i in (resp_json['analyzeResult']['pageResults'][0]['tables'][0]['cells'][0]):
-                    print(i)
+                for response in (resp_json['analyzeResult']['pageResults'][0]['tables'][0]['cells']):
+                    text.append(response['text'])
+
                 #pprint("Layout Analysis succeeded:\n%s" % resp_json)
                 quit()
             if status == "failed":
@@ -52,4 +51,8 @@ def x():
             msg = "GET analyze results failed:\n%s" % str(e)
             print(msg)
             quit()
-    return data
+    return text
+
+
+text = getTotal()
+print(text)
