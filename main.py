d49159e963374f3679d5ce0f4d685a123036ce98
kylejava <kyleedwardgjava@gmail.com>
2021-01-04 17:51:20 -0800

    Need to update recognizer

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import json
 import requests
-
+from recognizer import *
 from flask import Flask, render_template, flash, request
 app = Flask(__name__)
 
 @app.route('/' , methods = ['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    data = []
+    data = x()
+    return render_template('index.html' , data = data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- recognizer.py
+++ recognizer.py
@@ -3,49 +3,53 @@ import time
 from requests import get, post
 from keys import *
 from pprint import pprint
-headers = {
-    # Request headers
-    'Content-Type': 'application/pdf',
-    'Ocp-Apim-Subscription-Key': apim_key,
-}
 
-with open(source, "rb") as f:
-    data_bytes = f.read()
+def x():
+    headers = {
+        # Request headers
+        'Content-Type': 'application/pdf',
+        'Ocp-Apim-Subscription-Key': apim_key,
+    }
+
+    with open(source, "rb") as f:
+        data_bytes = f.read()
 
 
-try:
-    resp = post(url = post_url, data = data_bytes, headers = headers)
-    if resp.status_code != 202:
-        print("POST analyze failed:\n%s" % resp.text)
-        quit()
-    print("POST analyze succeeded:\n%s" % resp.headers)
-    get_url = resp.headers["operation-location"]
-except Exception as e:
-    print("POST analyze failed:\n%s" % str(e))
-    quit()
-
-n_tries = 10
-n_try = 0
-wait_sec = 6
-while n_try < n_tries:
     try:
-        resp = get(url = get_url, headers = {"Ocp-Apim-Subscription-Key": apim_key})
-        resp_json = json.loads(resp.text)
-        if resp.status_code != 200:
-        #    pprint("GET Layout results failed:\n%s" % resp_json)
+        resp = post(url = post_url, data = data_bytes, headers = headers)
+        if resp.status_code != 202:
+            print("POST analyze failed:\n%s" % resp.text)
             quit()
-        status = resp_json["status"]
-        if status == "succeeded":
-            pprint(resp_json['analyzeResult']['readResults']["lines"])
-            #pprint("Layout Analysis succeeded:\n%s" % resp_json)
-            quit()
-        if status == "failed":
-        #    pprint("Layout Analysis failed:\n%s" % resp_json)
-            quit()
-        # Analysis still running. Wait and retry.
-        time.sleep(wait_sec)
-        n_try += 1
+        print("POST analyze succeeded:\n%s" % resp.headers)
+        get_url = resp.headers["operation-location"]
     except Exception as e:
-        msg = "GET analyze results failed:\n%s" % str(e)
-        print(msg)
+        print("POST analyze failed:\n%s" % str(e))
         quit()
+    data = []
+    n_tries = 10
+    n_try = 0
+    wait_sec = 6
+    while n_try < n_tries:
+        try:
+            resp = get(url = get_url, headers = {"Ocp-Apim-Subscription-Key": apim_key})
+            resp_json = json.loads(resp.text)
+            if resp.status_code != 200:
+            #    pprint("GET Layout results failed:\n%s" % resp_json)
+                quit()
+            status = resp_json["status"]
+            if status == "succeeded":
+                for i in (resp_json['analyzeResult']['pageResults'][0]['tables'][0]['cells'][0]):
+                    print(i)
+                #pprint("Layout Analysis succeeded:\n%s" % resp_json)
+                quit()
+            if status == "failed":
+            #    pprint("Layout Analysis failed:\n%s" % resp_json)
+                quit()
+            # Analysis still running. Wait and retry.
+            time.sleep(wait_sec)
+            n_try += 1
+        except Exception as e:
+            msg = "GET analyze results failed:\n%s" % str(e)
+            print(msg)
+            quit()
+    return data
